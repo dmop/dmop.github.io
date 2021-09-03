8d68cb9c19094f650fc5cae0762066f31ce69754
Danilo Pedrosa <danilomopedrosa@gmail.com>
2021-09-03 11:18:54 -0300

    feat(add-barcode-preview): improve tests

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 async function prt() {
-    function printPartOfPage(elementId, uniqueIframeId){
-        const content = document.getElementById(elementId)
-        let pri
-        if (document.getElementById(uniqueIframeId)) {
-            pri = document.getElementById(uniqueIframeId).contentWindow
-        } else {
-            const iframe = document.createElement('iframe')
-            iframe.setAttribute('title', uniqueIframeId)
-            iframe.setAttribute('id', uniqueIframeId)
-            iframe.setAttribute('style', 'height: 0px; width: 0px; position: absolute;')
-            document.body.appendChild(iframe)
-            pri = iframe.contentWindow
-        }
-        pri.document.open()
-        pri.document.write(content.innerHTML)
-        pri.document.close()
-        pri.focus()
-        pri.print()
-    }
-
-    printPartOfPage("table", "ifmcontentstoprint");
+    window.print()
 }
 
 function printModal() {
@@ -61,6 +41,6 @@ function printModal() {
     downloadBlob(blob);
 }
 
-
+JsBarcode("#barcode", "Hi world!");
 document.getElementById("test-blob").addEventListener("click", prt);
 document.getElementById("test-modal").addEventListener("click", printModal);
